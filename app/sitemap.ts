99ee2f6b92607d52f5f9d065009b724a5e2beb69
Nikhil Gupta <inbox.nikhilgupta@gmail.com>
2026-06-25 13:57:28 +0530

    static link removal

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -32,7 +32,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = [
     "/",
     "/discover",
-    "/on-road-price",
     "/photos",
     "/dealer",
     "/dealer/login",
